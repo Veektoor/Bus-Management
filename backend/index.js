4b59aae3e8e8973ea427e0bbd38309c542c2b5b7
veektoor <gichuhivictorm@gmail.com>
2024-11-04 12:23:52 +0300

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Logging Middleware
+app.use((req, res, next) => {
+    const currentDateTime = new Date().toISOString();
+    console.log(`[${currentDateTime}] ${req.method} ${req.originalUrl}`);
+    next(); // Call the next middleware/route handler
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected")).catch(err => console.log(err));
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // Routes
 const busRoutes = require('./routes/busRoutes.js');
--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const DriverSchema = new mongoose.Schema({
     name: String,
     licenseNumber: String,
+    dateHired: {type:Date, default:Date.now()},
     assignedBus: { type: mongoose.Schema.Types.ObjectId, ref: 'Bus' }
 });
 
