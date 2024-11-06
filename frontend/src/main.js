f948fe15878f1c1a5a1190adcaca1b3a7904ba3d
DeeMKay <katisyadana@gmail.com>
2024-11-06 12:40:40 +0300

    main troubleshoot

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import Home from './views/Home.vue';
+import BusList from './components/BusList.vue';
+import DriverList from './components/DriverList.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/buses', component: BusList },
+    { path: '/drivers', component: DriverList },
+    // More routes for Routes and Fares
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
