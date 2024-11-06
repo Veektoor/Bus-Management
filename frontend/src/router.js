be05398d74d13d708a1a9b4ebce60b654797d047
veektoor <gichuhivictorm@gmail.com>
2024-11-06 15:46:28 +0300

    until tomorrow

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,13 +3,15 @@ import Home from './views/Home.vue';
 import BusList from './components/BusList.vue';
 import DriverList from './components/DriverList.vue'; 
 import NotFound from './views/NotFound.vue';
+import RoutesComponent from './components/RouteList.vue';
+import FaresComponent from './components/FareList.vue';
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/buses', name: 'buses', component: BusList },
   { path: '/drivers', name: 'drivers', component: DriverList },
-//   { path: '/routes', name: 'routes', component: RoutesComponent }, // Placeholder for routes page
-//   { path: '/fares', name: 'fares', component: FaresComponent }, // Placeholder for fares page
+  { path: '/routes', name: 'routes', component: RoutesComponent }, 
+  { path: '/fares', name: 'fares', component: FaresComponent }, 
   { path: '/:pathMatch(.*)*', name: 'notfound', component: NotFound }, // 404 route
 ];
 
