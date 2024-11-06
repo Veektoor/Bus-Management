2f68d911235a5f78efe9b5e5b9eb7a2629b77507
veektoor <gichuhivictorm@gmail.com>
2024-11-06 12:22:21 +0300

    frontend added

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // Import router.js
-
-createApp(App)
-  .use(router)  // Register the router
-  .mount('#app');  // Mount the app
-
